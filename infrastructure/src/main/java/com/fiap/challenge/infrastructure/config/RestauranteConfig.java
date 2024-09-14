93d18dd6e42c6189934cec2ffb629ea0b969d904
luizfilipebp <luizfilipe098@gmail.com>
2024-09-14 16:15:36 -0300

    configuração do caso de uso

---
+++ infrastructure/src/main/java/com/fiap/challenge/infrastructure/config/RestauranteConfig.java
@@ -0,0 +1,25 @@
+package com.fiap.challenge.infrastructure.config;
+
+import com.fiap.challenge.application.gateway.BuscarRestauranteGateway;
+import com.fiap.challenge.application.gateway.CadastrarRestauranteGateway;
+import com.fiap.challenge.application.usecaseimpl.restaurante.BuscarRestauranteUseCaseImp;
+import com.fiap.challenge.application.usecaseimpl.restaurante.CadastrarRestauranteUseCaseImpl;
+import com.fiap.challenge.usecase.restaurante.BuscarRestauranteUseCase;
+import com.fiap.challenge.usecase.restaurante.CadastrarRestauranteUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RestauranteConfig {
+    @Bean
+    BuscarRestauranteUseCase buscarRestauranteUseCase(BuscarRestauranteGateway restauranteGateway){
+        return new BuscarRestauranteUseCaseImp(restauranteGateway);
+    }
+
+
+    @Bean
+    CadastrarRestauranteUseCase cadastrarRestauranteUseCase(CadastrarRestauranteGateway restauranteGateway){
+        return new CadastrarRestauranteUseCaseImpl(restauranteGateway);
+    }
+
+}
