d39fbb6a2e157e9d9cb0f48e15967c539ec92570
luizfilipebp <luizfilipe098@gmail.com>
2024-09-14 16:14:08 -0300

    adicionando o repository de restaurante

---
+++ infrastructure/src/main/java/com/fiap/challenge/infrastructure/repository/RestauranteRepository.java
@@ -0,0 +1,19 @@
+package com.fiap.challenge.infrastructure.repository;
+
+import com.fiap.challenge.core.model.enums.TipoCozinha;
+import com.fiap.challenge.infrastructure.entity.RestauranteEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RestauranteRepository extends JpaRepository<RestauranteEntity, Long> {
+    Page<RestauranteEntity> findByNomeContaining(String nome, Pageable pegeable);
+    List<RestauranteEntity> findAllByLocalizacao_LogradouroContaining(String logradouro);
+    List<RestauranteEntity> findAllByLocalizacao_BairroContaining(String logradouro);
+    List<RestauranteEntity> findAllByLocalizacao_CidadeContaining(String logradouro);
+    List<RestauranteEntity> findByTipoCozinha(TipoCozinha tipoCozinha);
+}
